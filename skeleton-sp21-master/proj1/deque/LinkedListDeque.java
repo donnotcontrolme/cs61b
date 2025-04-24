c72461d23493b2b0deaa2f1a37b7e2ee4426015b
pasa <152273140+donnotcontrolme@users.noreply.github.com>
2025-04-24 18:41:12 +0800

    Revert "Delete skeleton-sp21-master directory"
    This reverts commit 3f2cbd7a4778c3bacce60290f34bfdabe9975ae7.

---
+++ skeleton-sp21-master/proj1/deque/LinkedListDeque.java
@@ -0,0 +1,17 @@
+package deque;
+
+public class LinkedListDeque
+{
+    public static class IntList
+    {
+        private int first;
+        private IntList next;
+
+        public IntList(int f,IntList n)
+        {
+            first=f;
+            next=n;
+        }
+    }
+
+}
